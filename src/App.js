c35b31b57e9a7b1487c777c7ef18ab7135b08ef1
Homero Batista Kzam <homero@parkcode.mobi>
2019-06-01 11:23:37 -0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, { useState, useRef, useMemo } from 'reactn';
 import logo from './logo.svg';
 import './App.css';
 
+import { Button, Template, Form, FileUploader } from 'devextreme-react';
+import { SimpleItem, Item, RequiredRule } from 'devextreme-react/form';
+
 function App() {
+
+  const [currentData, setCurrentData] = useState({ nome: null, filename: null });
+
+  const refButtonImportar = useRef();
+
+  const uploadUrl = 'https://js.devexpress.com/Content/Services/upload.aspx';
+  console.log(uploadUrl);
+
+  const handleOnUploaded = (e) => {
+    setCurrentData({ ...currentData, filename: e.request.responseText });
+  }
+
+  const handleOnClickImportar = (e) => {
+    console.log("handleOnClickImportar", e);
+  }
+
+  const memoizedFileUploader = useMemo(() =>
+
+    <FileUploader selectButtonText='File' accept={'image/*'} multiple={false} uploadMode={'instantly'} uploadUrl={uploadUrl} onUploaded={handleOnUploaded} />
+
+    , [uploadUrl]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form formData={currentData} labelLocation={'top'} >
+
+        <SimpleItem dataField={'nome'} label={ {text: 'Nome'}} >
+          <RequiredRule message={"Nome é obrigatório!"} />
+        </SimpleItem>
+
+        <Item template={'fileUploaderTemplate'} />
+        <Template name={'fileUploaderTemplate'}>
+          { memoizedFileUploader }
+        </Template>
+
+        <Item template={'buttonsTemplate'} />
+        <Template name={'buttonsTemplate'}>
+          <Button ref={refButtonImportar} text={'Importar'} type={'success'} icon={'save'} disabled={!currentData.filename} onClick={handleOnClickImportar} />
+        </Template>
+
+      </Form>
     </div>
   );
 }
